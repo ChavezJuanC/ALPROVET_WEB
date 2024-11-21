b494a5275400fb5a2665f2a4d471d83f5c6a67c0
SKRPTZ <43616880+ChavezJuanC@users.noreply.github.com>
2024-11-21 00:14:35 -0600

    Batman

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default App;
+
+//create routing
+//create routes
+//create nav bar component (PROTOTYPE)
+//set custom color pallet
+//set BG
+//set icon to change themes
+//review flex and postioning with tailwind css
+//create responsive navbar
+//add routing to the navbar
+
+//start with homepage
+//find a maps service
+//create hero section
+//create service content section
+//create location and contact contact section
+//create footer section
